92ed7cc19c53c6999cddd6a591a52c7059803b2d
lashatatkhashvili <lasha.tatkhashvili@yahoo.com>
2021-07-11 23:16:34 +0400

    frontend: fix imports

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import CatForm from "./Front-end/Form/form";
-import CatSignForm from "./Front-end/Form/SIgnUp";
+import CatForm from "./Form/form";
+import CatSignForm from "./Form/SIgnUp";
 function App() {
   return (
     <div className="App">
